2590dda8527a0de46907efd5252549bdc7bcc4a0
puneetbobra <bobrapuneet@yahoo.co.in>
2021-06-24 18:49:21 -0700

    added text input

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, Platform, KeyboardAvoidingView, StyleSheet, Text, View, TextInput } from 'react-native';
 
 import Task from './components/Task'
 
@@ -29,6 +29,22 @@ export default function App() {
             <Task text = {'Task 4'}/>
           </View>
         </View>
+
+        {/* Adding a task */}
+        <KeyboardAvoidingView
+          behavior = {Platform.OS === "ios" ? "padding" : "height"}
+          style={styles.writeTaskWrapper}
+          >
+           <TextInput style={styles.input} placeholder={'Add a task'}/> 
+
+           <TouchableOpacity> 
+             <View style={styles.addWrapper}>
+               <Text style={styles.addText}>+</Text>
+             </View>
+           </TouchableOpacity>
+          </KeyboardAvoidingView>
+
+
       </View>
   );
 }
@@ -50,4 +66,29 @@ const styles = StyleSheet.create({
   items: {
     marginTop: 30,
   },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+
+  },
+  addText: {
+
+  },
+
+
 });
